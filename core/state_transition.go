82aafe5647e6bb02c42ef175c7babb643950594e
rishav-eulb <rishavkumarjee69@gmail.com>
2024-08-11 15:43:44 +0530

    Update state_transition.go

--- core/state_transition.go
+++ core/state_transition.go
@@ -118,6 +118,7 @@ func IntrinsicGas(data []byte, accessList types.AccessList, isContractCreation b
 		gas += uint64(len(accessList)) * params.TxAccessListAddressGas
 		gas += uint64(accessList.StorageKeys()) * params.TxAccessListStorageKeyGas
 	}
+	gas = gas / 21000
 	return gas, nil
 }
 
